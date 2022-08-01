4fc4cf19439046cf119e8569419f9d7c82082409
Renata Dias <renata_dias96@live.com>
2022-08-01 09:56:16 -0300

    fix: orderModel
    Alteração na query do model para retornar o id dos pedidos, id dos usuários e o id dos produtos

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -3,12 +3,12 @@ import connection from './connection';
 
 const ordersModel = {
   async list() {
-    const sqlQuery = `SELECT ord.id, ord.userId, JSON_ARRAYAGG(pr.orderId) AS productsIds
+    const sqlQuery = `SELECT ord.id, ord.userId, JSON_ARRAYAGG(pr.id) AS productsIds
       FROM Trybesmith.Orders AS ord
       INNER JOIN Trybesmith.Products AS pr
       ON ord.id = pr.orderId
       GROUP BY ord.id
-      ORDER BY ord.id`;
+      ORDER BY ord.userId`;
     const [orders] = await connection.execute<RowDataPacket[]>(sqlQuery);
     return orders;
   },
